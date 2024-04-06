2fcae20a74f5f6885fb1b2db733955f1e867ad14
iamjooon2 <iamjooon2@gmail.com>
2024-04-07 02:02:20 +0900

    feat: 로그인 페이지 기능 구현

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,43 +1,70 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 export default function Login() {
+  
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkLogin, setCheckLogin] = useState(false);
+
+  const handleLogin = ( ) => {
+
+    const requestBody = {
+      id : id,
+      password : password
+    };
+
+    const response = axios.post("https://dev.bookbla.shop/api/admin/auth/login", requestBody)
+    .then(response => {      
+      if (response.status === 200) {
+        setCheckLogin(true);
+        navigate(`/`);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
   return (
-    <Conatiner>
-      <LoginForm>
-        <Form action="/admin/login" method="post">
-            BookBLA Admin
+    <Container>
+      <Title>BookBLA Admin</Title>
+      <BookblaContainer>
           <div className="form-group">
-            <FormControl type="text" placeholder="username" required="required" name="username" />
+            <FormControl type="text" placeholder="id" required="required" name="id" value = {id} onChange={(e) => setId(e.target.value)}/>
+          </div>
+          <div>
+            <FormControl type="password" placeholder="password" required="required" name="password" value = {password} onChange={(e) => setPassword(e.target.value)}/>
           </div>
           <div className="form-group">
-            <FormControl type="password" placeholder="password" required="required" name="password" />
-          </div>
-          <div className="form-group">
-            <button type="submit">
+            <button onClick={handleLogin} type= "submit">
               로그인
             </button>
           </div>
-        </Form>
-      </LoginForm>
-    </Conatiner>
+      </BookblaContainer>
+    </Container>
   );
 };
 
-const Conatiner = styled.div`
-  display : flex;
-  justify-content: center;
-  align-items: center;
+const Container = styled.div`
+  margin: 8px auto 48px;
+  text-align: center;
+  font-size: 15px;
 `;
 
-const LoginForm = styled.div`
-  width: 340px;
+const Title = styled.h2`
+  margin: 0 0 24px;
+  text-align: center;
+  letter-spacing: -0.2px;
 `;
 
-
-const Form = styled.form`
-  margin-bottom: 15px;
-  padding: 30px;
+const BookblaContainer = styled.div`
+  font-size: 14px;
+  margin-bottom: 48px;
+  & > svg {
+    width: 100%;
+  }
 `;
 
 const FormControl = styled.input`
