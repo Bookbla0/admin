7a7df1a1e087d9fef9df000d66a3d0d3ffa713c9
iamjooon2 <iamjooon2@gmail.com>
2024-06-05 03:44:03 +0900

    feat: 대기 목록 조회시 역순 정렬 추가

--- src/api/member/member.api.js
+++ src/api/member/member.api.js
@@ -2,9 +2,9 @@ import { Get, Patch } from '@/utils/api/api';
 
 export const membersApi = (page = 1) => Get(`/members?page=${page}`);
 
-export const membersPendingProfileImage = (page = 1) => Get(`/members/pending/profile-image?page=${page}`);
-export const membersPendingStudentImage = (page = 1) => Get(`/members/pending/student-id/image?page=${page}`);
-export const membersPendingKakao = (page = 1) => Get(`/members/pending/kakao?page=${page}`);
+export const membersPendingProfileImage = (page = 1) => Get(`/members/pending/profile-image?page=${page}&sort=id,desc`);
+export const membersPendingStudentImage = (page = 1) => Get(`/members/pending/student-id/image?page=${page}&sort=id,desc`);
+export const membersPendingKakao = (page = 1) => Get(`/members/pending/kakao?page=${page}&sort=id,desc`);
 
 export const membersApprovalApi = (page = 1) => Get(`/members/approval?page=${page}&size=25`);
 
