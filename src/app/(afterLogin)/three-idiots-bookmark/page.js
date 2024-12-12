7a4056d412b20a5674de417c5d6e49ede6d95d47
eggmun <eggmun98@gmail.com>
2024-12-12 20:32:47 +0900

    진짜 마지막 배포

--- src/app/(afterLogin)/three-idiots-bookmark/page.js
+++ src/app/(afterLogin)/three-idiots-bookmark/page.js
@@ -6,9 +6,10 @@ import { useState } from 'react';
 
 export default function APage() {
   const [bookmarkCount, setBookmarkCount] = useState(0);
+  const [isBookmarkId, setIsBookmarkId] = useState(-1);
 
   const onClickCreateBookmarkCount = async (params) => {
-    if (!bookmarkCount || params.row.memberId) return alert('값이 비어 있어요');
+    if (!bookmarkCount) return alert('값이 비어 있어요');
 
     if (!window.confirm(`${params.row.memberId}의 유저에게 ${bookmarkCount}만큼 올려주시겠습니까?`)) return;
 
@@ -21,10 +22,13 @@ export default function APage() {
     } catch (error) {
       console.log('error: {}', error);
       alert('유저 북마크 수 올리는데에 실패하였습니다.');
+    } finally {
+      setBookmarkCount(0);
     }
   };
 
   const onChangeBookmarkCount = (e) => {
+    console.log('e: {}', e.target.value);
     setBookmarkCount(e.target.value);
   };
 
@@ -36,16 +40,26 @@ export default function APage() {
     {
       field: 'bookmarkCountInput',
       headerName: '올리고 싶은 북마크 수',
-      renderCell: () => {
-        return <input onChange={onChangeBookmarkCount} />;
+      renderCell: (params) => {
+        return isBookmarkId === params.row.memberBookmarkId ? (
+          <input onChange={onChangeBookmarkCount} />
+        ) : (
+          <button onClick={() => setIsBookmarkId(params.row.memberBookmarkId)}>편집</button>
+        );
       },
     },
     {
       field: 'action',
       headerName: '액션',
       renderCell: (params) => {
-        console.log('params: {}', params);
-        return <button onClick={() => onClickCreateBookmarkCount(params)}>적용</button>;
+        return (
+          isBookmarkId === params.row.memberBookmarkId && (
+            <>
+              <button onClick={() => onClickCreateBookmarkCount(params)}>적용</button>
+              <button onClick={() => setIsBookmarkId(-1)}>취소</button>
+            </>
+          )
+        );
       },
     },
   ];
